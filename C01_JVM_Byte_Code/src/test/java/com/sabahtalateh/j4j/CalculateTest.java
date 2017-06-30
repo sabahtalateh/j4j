92c11911db93c1d10284c8b8e19d08ed9de12e8e
Alexandr Kravtsov <ak@4xxi.com>
2017-06-30 13:13:53 +0300

    task 4122

---
+++ C01_JVM_Byte_Code/src/test/java/com/sabahtalateh/j4j/CalculateTest.java
@@ -0,0 +1,20 @@
+package com.sabahtalateh.j4j;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+
+import static org.junit.Assert.*;
+
+public class CalculateTest {
+
+    @Test
+    public void whenAddOneToOneThenGetTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(out.toString(), is(String.format("Hello%s", System.getProperty("line.separator"))));
+    }
+}
