b651048a94f0ab364ca6fb360ae6f6283f9c6496
Alexandr Kravtsov <ak@4xxi.com>
2017-07-14 18:00:50 +0300

    WIP

--- C03_Collections_Lite/src/main/java/com/sabahtalateh/j4j/collections_lite/list_performance_comparison/Comparison.java
+++ C03_Collections_Lite/src/main/java/com/sabahtalateh/j4j/collections_lite/list_performance_comparison/Comparison.java
@@ -1,99 +1,133 @@
 package com.sabahtalateh.j4j.collections_lite.list_performance_comparison;
 
-//import java.util.ArrayList;
-
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.UUID;
+import java.util.TreeSet;
+
+import static java.lang.String.format;
 
 /**
  * Comparison.
  */
 public class Comparison {
 
-    String[] randomStrings;
-
     /**
      * @param collection o add.
      * @param n          number of elements.
+     * @param br         reader.
      * @return time to add.
+     * @throws IOException or not.
      */
-    public long add(Collection<String> collection, int n) {
-        if (randomStrings == null) {
-            randomStrings = new String[n];
-            System.out.println("Start random strings generation..");
-            for (int i = 0; i < n; i++) {
-                this.randomStrings[i] = UUID.randomUUID().toString();
-            }
+    public long add(Collection<String> collection, int n, BufferedReader br) throws IOException {
+        String[] strings = new String[n];
+        for (int i = 0; i < n; i++) {
+            strings[i] = br.readLine();
         }
-        System.out.println("Start adding..");
+
         long start = System.currentTimeMillis();
         for (int i = 0; i < n; i++) {
-            collection.add(this.randomStrings[i]);
+            collection.add(strings[i]);
         }
-        System.out.println("End adding..");
         return System.currentTimeMillis() - start;
     }
 
     /**
      * @param collection to delete.
-     * @param n number of elements
+     * @param n          number of elements
+     * @param br         reader.
      * @return time to delete.
+     * @throws IOException or not.
      */
-    public long delete(Collection<String> collection, int n) {
-        System.out.println("Start deleting..");
+    public long delete(Collection<String> collection, int n, BufferedReader br) throws IOException {
+        String[] strings = new String[n];
+        for (int i = 0; i < n; i++) {
+            strings[i] = br.readLine();
+        }
+
         long start = System.currentTimeMillis();
         for (int i = 0; i < n; i++) {
-            collection.remove(this.randomStrings[i]);
+            collection.remove(strings[i]);
         }
-        System.out.println("End deleting..");
         return System.currentTimeMillis() - start;
     }
 
     /**
      * @param args of method.
+     * @throws IOException or not.
      */
-    public static void main(String[] args) {
-        Comparison comparison = new Comparison();
+    public static void main(String[] args) throws IOException {
+//        TestFileGenerator.generate("out.txt", (int) 1e8);
 
-        int addSize = (int) 10e8;
-        int deleteSize = (int) 10e2;
+        InputStream fis = new FileInputStream("out.txt");
+        InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+        BufferedReader br = new BufferedReader(isr);
 
-        Collection<String> arrayList = new ArrayList<>();
-        System.out.println("Array list");
-        System.out.println("Add: " + comparison.add(arrayList, addSize));
-        System.out.println("Delete: " + comparison.delete(arrayList, deleteSize));
 
-        Collection<String> linkedList = new LinkedList<>();
-        System.out.println("Linked list");
-        System.out.println("Add: " + comparison.add(linkedList, addSize));
-        System.out.println("Delete: " + comparison.delete(linkedList, deleteSize));
+        if (args[0].equals("linked_list")) {
+            System.out.println("Linked list:");
+            Collection<String> linkedList = new LinkedList<>();
+            int linkedListAddAmount = Integer.valueOf(args[1]);
+            long linkedListAddTime = new Comparison().add(linkedList, linkedListAddAmount, br);
+            System.out.println(format(
+                    "Add    1e%d els.   : %.3f seconds",
+                    String.valueOf(linkedListAddAmount).length() - 1,
+                    linkedListAddTime / 1000.0)
+            );
 
-//        Collection<String> treeSet = new TreeSet<>();
-//        System.out.println("Tree set");
-//        System.out.println("Add: " + comparison.add(treeSet, addSize));
+            int linkedListDeleteAmount = Integer.valueOf(args[2]);
+            long linkedListDeleteTime = new Comparison().delete(linkedList, linkedListDeleteAmount, br);
+            System.out.println(format(
+                    "Delete 1e%d els.   : %.3f seconds",
+                    String.valueOf(linkedListDeleteAmount).length() - 1,
+                    linkedListDeleteTime / 1000.0)
+            );
+        }
+
+        if (args[0].equals("array_list")) {
+            System.out.println("Array list:");
+            Collection<String> arrayList = new ArrayList<>();
+
+            int arrayListAddAmount = Integer.valueOf(args[1]);
+            long arrayListAddTime = new Comparison().add(arrayList, arrayListAddAmount, br);
+            System.out.println(format(
+                    "Add    1e%d els.   : %.3f seconds",
+                    String.valueOf(arrayListAddAmount).length() - 1,
+                    arrayListAddTime / 1000.0)
+            );
+
+
+            int arrayListDeleteAmount = Integer.valueOf(args[2]);
+            long arrayListDeleteTime = new Comparison().delete(arrayList, arrayListDeleteAmount, br);
+            System.out.println(format(
+                    "Delete 1e%d els.   : %.3f seconds",
+                    String.valueOf(arrayListDeleteAmount).length() - 1,
+                    arrayListDeleteTime / 1000.0)
+            );
+        }
+
+        if (args[0].equals("tree_set")) {
+            System.out.println("Tree set:");
+            Collection<String> treeSet = new TreeSet<>();
+
+            int treeSetAddAmount = Integer.valueOf(args[1]);
+            long treeSetAddTime = new Comparison().add(treeSet, treeSetAddAmount, br);
+            System.out.println(format(
+                    "Add    1e%d els.   : %.3f seconds",
+                    String.valueOf(treeSetAddAmount).length() - 1,
+                    treeSetAddTime / 1000.0)
+            );
+
+
+            int treeSetDeleteAmount = Integer.valueOf(args[2]);
+            long arrayListDeleteTime = new Comparison().delete(treeSet, treeSetDeleteAmount, br);
+            System.out.println(format(
+                    "Delete 1e%d els.   : %.3f seconds",
+                    String.valueOf(treeSetDeleteAmount).length() - 1,
+                    arrayListDeleteTime / 1000.0)
+            );
+        }
     }
-
-//    public static void main(String[] args) {
-//        int size = (int) 10e6;
-//
-//        System.out.print(String.format("Max: %.3fGiB%n", Runtime.getRuntime().maxMemory() / (1024.0 * 1024.0 * 1024.0)));
-//        System.out.print(String.format("Total: %.3fGiB%n", Runtime.getRuntime().totalMemory() / (1024.0 * 1024.0 * 1024.0)));
-//        System.out.print(String.format("Free: %.3fGiB%n", Runtime.getRuntime().freeMemory() / (1024.0 * 1024.0 * 1024.0)));
-//        long mem = Runtime.getRuntime().freeMemory();
-//
-//        List<String> arrayList = new LinkedList<>();
-//        for (int i = 0; i < size; i++) {
-//            arrayList.add("Long long long long long long long long string " + i);
-//        }
-//
-//        System.out.print(String.format("Max: %.3fGiB%n", Runtime.getRuntime().maxMemory() / (1024.0 * 1024.0 * 1024.0)));
-//        System.out.print(String.format("Total: %.3fGiB%n", Runtime.getRuntime().totalMemory() / (1024.0 * 1024.0 * 1024.0)));
-//        System.out.print(String.format("Free: %.3fGiB%n", Runtime.getRuntime().freeMemory() / (1024.0 * 1024.0 * 1024.0)));
-//
-//        long memUsed = mem - Runtime.getRuntime().freeMemory();
-//        System.out.print(String.format("Used: %.3fGiB%n", memUsed / (1024.0 * 1024.0 * 1024.0)));
-//
-//    }
 }
