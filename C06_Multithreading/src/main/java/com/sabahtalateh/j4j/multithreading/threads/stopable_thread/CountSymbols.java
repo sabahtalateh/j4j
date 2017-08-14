3118d69127cd23af096a4026792164ca1b5fac19
Alexandr Kravtsov <ak@4xxi.com>
2017-08-14 20:54:51 +0300

    WIP.

---
+++ C06_Multithreading/src/main/java/com/sabahtalateh/j4j/multithreading/threads/stopable_thread/CountSymbols.java
@@ -0,0 +1,8 @@
+package com.sabahtalateh.j4j.multithreading.threads.stopable_thread;
+
+/**
+ * CountSymbols.
+ */
+public class CountSymbols {
+
+}
---
+++ C06_Multithreading/src/main/java/com/sabahtalateh/j4j/multithreading/threads/stopable_thread/SymbolsCounter.java
@@ -0,0 +1,14 @@
+package com.sabahtalateh.j4j.multithreading.threads.stopable_thread;
+
+/**
+ * SymbolsCounter.
+ */
+public class SymbolsCounter implements Runnable {
+    /**
+     * Run process.
+     */
+    @Override
+    public void run() {
+
+    }
+}
