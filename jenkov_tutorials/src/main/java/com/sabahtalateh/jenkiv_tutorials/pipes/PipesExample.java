5a80b20af57e7590132c74efd88c3f551844fb3f
Alexandr Kravtsov <ak@4xxi.com>
2017-10-24 16:31:35 +0300

    Jenkov Pipes.

---
+++ jenkov_tutorials/src/main/java/com/sabahtalateh/jenkiv_tutorials/pipes/PipesExample.java
@@ -0,0 +1,39 @@
+package com.sabahtalateh.jenkiv_tutorials.pipes;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+/**
+ * PipesExample.
+ */
+public class PipesExample {
+    /**
+     * @param args args.
+     * @throws IOException exception.
+     */
+    public static void main(String[] args) throws IOException {
+        PipedOutputStream pipedOutputStream = new PipedOutputStream();
+        PipedInputStream pipedInputStream = new PipedInputStream();
+        pipedOutputStream.connect(pipedInputStream);
+
+        new Thread(() -> {
+            try {
+                pipedOutputStream.write("Hello world!".getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                int d = pipedInputStream.read();
+                while (d != -1) {
+                    System.out.printf("%s", (char) d);
+                    d = pipedInputStream.read();
+                }
+            } catch (IOException e) {
+            }
+        }).start();
+    }
+}
