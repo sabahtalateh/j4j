38039599cb7a1f7869d493f9f50344f92cd0cebb
Alexandr Kravtsov <ak@4xxi.com>
2017-12-09 13:45:34 +0300

    WIP

---
+++ asm/src/main/java/com/sabahtalateh/asm/Foo.java
@@ -0,0 +1,7 @@
+package com.sabahtalateh.asm;
+
+/**
+ * Created by ak-4xxi on 07/12/2017.
+ */
+public class Foo {
+}
---
+++ asm/src/main/java/com/sabahtalateh/asm/SimpleMethod.java
@@ -0,0 +1,7 @@
+package com.sabahtalateh.asm;
+
+/**
+ * Created by ak-4xxi on 08/12/2017.
+ */
+public class Simplemethod {
+}
