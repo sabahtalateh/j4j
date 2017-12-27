8af45b1e45e88ca9b81298d5a318f374ac2c9e41
Alexandr Kravtsov <sabahtalateh@gmail.com>
2017-12-27 16:40:15 +0300

    jdbc xml xslt fix.

--- C07_SQL_JDBC/src/main/java/com/sabahtalateh/j4j/jdbc/xml_xslt_jdbc/Runner.java
+++ C07_SQL_JDBC/src/main/java/com/sabahtalateh/j4j/jdbc/xml_xslt_jdbc/Runner.java
@@ -1,5 +1,7 @@
 package com.sabahtalateh.j4j.jdbc.xml_xslt_jdbc;
 
+import org.apache.commons.io.FileUtils;
+
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
@@ -12,8 +14,12 @@ import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.sql.*;
+import java.util.UUID;
+
+import static java.lang.String.format;
 
 /**
  * Runner.
@@ -32,8 +38,8 @@ public class Runner {
      * @param n            number of rows to insert.
      */
     public Runner(String SQLiteDBPath, Path XMLOutputDir, int n) {
-        this.SQLiteDBConnectionString = String.format("jdbc:sqlite:%s", SQLiteDBPath);
-        this.XMLOutputDir = XMLOutputDir;
+        this.SQLiteDBConnectionString = format("jdbc:sqlite:%s", SQLiteDBPath);
+        this.XMLOutputDir = XMLOutputDir.toAbsolutePath();
         this.n = n;
     }
 
@@ -52,7 +58,10 @@ public class Runner {
      * Create SQLite DB and fill it with numbers.
      */
     private void createAndFillDB() {
-        try (Connection connection = DriverManager.getConnection(this.SQLiteDBConnectionString)) {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(this.SQLiteDBConnectionString);
 
             try (Statement statement = connection.createStatement()) {
                 statement.executeUpdate("CREATE TABLE IF NOT EXISTS test (field INTEGER);");
@@ -69,10 +78,25 @@ public class Runner {
                 preparedStatement.executeBatch();
             }
 
-            connection.setAutoCommit(true);
-
+            connection.commit();
         } catch (SQLException e) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
             e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.setAutoCommit(true);
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
@@ -115,12 +139,26 @@ public class Runner {
      * @throws TransformerException exception.
      */
     private void transformXML() throws TransformerException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource("xslt/transform.xslt");
+        UUID uuid = UUID.randomUUID();
+        // Copy xslt to regular file because of some issue with Transformer
+        // that rices a malformed url exception when trying to load resource from
+        // within a jar file.
+        File temporaryFile = new File(format("/tmp/%s.xslt", uuid.toString()));
+        try {
+            FileUtils.copyURLToFile(resource, temporaryFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         TransformerFactory factory = TransformerFactory.newInstance();
-        Source xslt = new StreamSource(new File("src/main/resources/xslt/transform.xslt"));
+        Source xslt = new StreamSource(temporaryFile);
         Transformer transformer = factory.newTransformer(xslt);
 
         Source text = new StreamSource(new File(XMLOutputDir.toFile(), "1.xml"));
         transformer.transform(text, new StreamResult(new File(XMLOutputDir.toFile(), "2.xml")));
+        temporaryFile.delete();
     }
 
 }
