392c2de6f5dfe7d71ce1de971da6cb17fb5102cc
Alexandr Kravtsov <sabahtalateh@gmail.com>
2018-04-17 02:39:22 +0300

    Create web-project structure.

---
+++ C08_Servlets/src/main/java/com/sabahtalateh/servlets/IntroServlet.java
@@ -0,0 +1,28 @@
+package com.sabahtalateh.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * IntroServlet.
+ */
+public class IntroServlet extends HttpServlet {
+
+    /**
+     * @param req  request.
+     * @param resp response.
+     * @throws ServletException exception.
+     * @throws IOException      exception.
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = new PrintWriter(resp.getOutputStream());
+        printWriter.append("Hello World!");
+        printWriter.flush();
+    }
+}
