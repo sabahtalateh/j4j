8f6b9a31805c3f3a4deed2a849c58fc4213d1e1b
Alexandr Kravtsov <ak@4xxi.com>
2018-06-25 19:09:36 +0300

    Play with Servlets

--- C08_Servlets/src/main/java/com/sabahtalateh/servlets/IntroServlet.java
+++ C08_Servlets/src/main/java/com/sabahtalateh/servlets/IntroServlet.java
@@ -6,11 +6,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * IntroServlet.
  */
 public class IntroServlet extends HttpServlet {
+
+    private List<String> users = new CopyOnWriteArrayList<>();
 
     /**
      * @param req  request.
@@ -21,8 +25,22 @@ public class IntroServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
+        String login = req.getParameter("login");
         PrintWriter printWriter = new PrintWriter(resp.getOutputStream());
-        printWriter.append("Hello World!");
+        printWriter.append(users.toString());
         printWriter.flush();
+    }
+
+    /**
+     * @param req  request.
+     * @param resp response.
+     * @throws ServletException exception.
+     * @throws IOException      exception.
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        users.add(req.getParameter("name"));
+        resp.setContentType("text/html");
+        resp.getOutputStream().println("OK");
     }
 }
